732fece502762a92ac7376cf8f6b1c4dc91ba1c4
kuku20 <lluu3790@gmail.com>
2024-08-13 15:54:32 -0500

    setup

--- javascript/0001-two-sum.js
+++ javascript/0001-two-sum.js
@@ -24,39 +24,6 @@ var twoSum = (nums, target) => {
 }
 
 console.log(twoSum(input,tar))
-/**
- * Hash Map - 2 Pass
- * Time O(N) | Space O(N)
- * https://leetcode.com/problems/two-sum/
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = (nums, target) => {
-    const map = getMap(nums);       /* Time O(N) | Space O(N) */
-
-    return getSum(nums, target, map)/* Time O(N) */
-}
-
-const getMap = (nums, map = new Map()) => {
-    for (let index = 0; index < nums.length; index++) {/* Time O(N) */
-        map.set(nums[index], index);                        /* Space O(N) */
-    }
-
-    return map
-}
-
-const getSum = (nums, target, map) => {
-    for (let index = 0; index < nums.length; index++) {/* Time O(N) */
-        const complement = target - nums[index];
-        const sumIndex = map.get(complement);
-
-        const isTarget = map.has(complement) && (map.get(complement) !== index)
-        if (isTarget) return [ index, sumIndex ]
-    }
-
-    return [ -1, -1 ];
-}
 
 /**
  * Hash Map - 1 Pass
@@ -80,3 +47,4 @@ var twoSum = (nums, target, map = new Map()) => {
 
     return [ -1, -1 ];
 }
+console.log(twoSum(input,tar))
